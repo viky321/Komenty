ca85f4731f6528160a8cccc18d0a30b0d0b51d9c
Viktoria Balascakova <viktoria.balascakova@gmail.com>
2024-06-08 22:57:55 +0200

    change name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Vimma') }}</title>
+    <title>{{ config('app.name', 'Komenty') }}</title>
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css?family=Nunito">
 
